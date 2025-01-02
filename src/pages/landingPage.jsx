ddf6e1d66308a3b94df0beced804bb186ae20d50
Reyhan Maulana <144551736+Rerey155-del@users.noreply.github.com>
2025-01-02 23:54:21 +0700

    oke

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -1,20 +1,9 @@
+
 const landingPage = () => {
     return (
         <section>
-            <div className="" style={{
-                backgroundColor:"#FBF8EF",
-                minHeight: "100vh",
-                width: "100%",
-                margin: "0",
-                padding: "0",
-                boxSizing: "border-box",
-                overflowX: "hidden",
-                backgroundImage: `url('https://res.cloudinary.com/dbc7scew4/image/upload/v1733543685/glow_effect_odggcl.png')`,
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: "center",
-                backgroundSize: "cover",
-                fontFamily: "Montserrat",
-            }}>
+            <div className="">
+              
                 <p>haiiiiidddddddddddddddddddddddddddddddddddddddddddddddddd</p>
             </div>
         </section>
