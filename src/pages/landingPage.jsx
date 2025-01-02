ba7d0b0e7d7a409155703d63c20cb52b09bade9f
Reyhan Maulana <144551736+Rerey155-del@users.noreply.github.com>
2025-01-02 19:22:50 +0700

    oke

---
+++ src/pages/landingPage.jsx
@@ -0,0 +1,24 @@
+const landingPage = () => {
+    return (
+        <section>
+            <div className="" style={{
+                backgroundColor:"#FBF8EF",
+                minHeight: "100vh",
+                width: "100%",
+                margin: "0",
+                padding: "0",
+                boxSizing: "border-box",
+                overflowX: "hidden",
+                backgroundImage: `url('https://res.cloudinary.com/dbc7scew4/image/upload/v1733543685/glow_effect_odggcl.png')`,
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+                fontFamily: "Montserrat",
+            }}>
+
+            </div>
+        </section>
+    )
+}
+
+export default landingPage
