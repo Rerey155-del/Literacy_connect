0e79ec8b20a5d91483c0f94d7f75dc3d3db5995e
Reyhan Maulana <144551736+Rerey155-del@users.noreply.github.com>
2025-01-04 12:09:52 +0700

    oke

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -100,6 +100,12 @@ const LandingPage = () => {
                             </div>
                         </div>
                     </div>
+
+                    <ul className="menu menu-vertical lg:menu-horizontal bg-base-200 rounded-box">
+                        <li><a>Item 1</a></li>
+                        <li><a>Item 2</a></li>
+                        <li><a>Item 3</a></li>
+                    </ul>
                 </div>
             </div>
 
